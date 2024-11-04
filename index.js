58084af8459812c2a0e7f751ff2332de84a8b8ee
James Abaja <jgabaja@gmail.com>
2024-11-04 15:08:43 +0800

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
+
 function convertTimeToWords(time) {
   // TODO: real code goes here!
-  if (time === '0:00') {
-    return 'midnight';
-  }
+  const digitToWords = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    21: "twenty one",
+    22: "twenty two",
+    23: "twenty three",
+    24: "twenty four",
+    25: "twenty five",
+    26: "twenty six",
+    27: "twenty seven",
+    28: "twenty eight",
+    29: "twenty nine",
+  };
 
-  return 'half past eight';
+  if (!time.includes(":")) return "Invalid time";
+
+  const timeArray = time.split(":");
+  const hour = timeArray[0];
+  const minutes = timeArray[1];
+  const intMinutes = parseInt(minutes);
+
+  switch (true) {
+    case time === "0:00":
+      return "midnight";
+    case time === "12:00":
+      return "midday";
+    case minutes === "00":
+      return `${digitToWords[parseInt(hour)]} o'clock`;
+    case minutes === "15":
+      return `quarter past ${digitToWords[parseInt(hour)]}`;
+    case minutes === "30":
+      return `half past ${digitToWords[parseInt(hour)]}`;
+    case minutes === "45":
+      return `quarter to ${digitToWords[parseInt(hour) + 1]}`;
+    case (parseInt(minutes) < 15 && parseInt(minutes) > 0) ||
+      (parseInt(minutes) > 15 && parseInt(minutes) < 30):
+      return `${digitToWords[intMinutes]} past ${digitToWords[parseInt(hour)]}`;
+    case (parseInt(minutes) < 45 && parseInt(minutes) > 30) ||
+      (parseInt(minutes) > 45 && parseInt(minutes) < 60):
+      return `${digitToWords[60 - intMinutes]} to ${
+        digitToWords[parseInt(hour) + 1]
+      }`;
+    default:
+      return "half past eight";
+  }
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -1,18 +1,53 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require("./index");
 
-describe('Time to words', () => {
-  it('Handles midnight', () => {
-    const timeInWords = convertTimeToWords('0:00');
-    expect(timeInWords).toBe('midnight');
+describe("Time to words", () => {
+  it("Handles midnight", () => {
+    const timeInWords = convertTimeToWords("0:00");
+    expect(timeInWords).toBe("midnight");
   });
 
-  it('Handles 30 - 8:30', () => {
-    const timeInWords = convertTimeToWords('8:30');
-    expect(timeInWords).toBe('half past eight');
+  it("Handles midday", () => {
+    const timeInWords = convertTimeToWords("12:00");
+    expect(timeInWords).toBe("midday");
   });
 
-  it('Handles times after 30 mins - 2:45', () => {
-    const timeInWords = convertTimeToWords('2:45');
-    expect(timeInWords).toBe('quarter to three');
+  it("Handles 2:00", () => {
+    const timeInWords = convertTimeToWords("2:00");
+    expect(timeInWords).toBe("two o'clock");
+  });
+
+  it("Handles 2:15", () => {
+    const timeInWords = convertTimeToWords("2:15");
+    expect(timeInWords).toBe("quarter past two");
+  });
+
+  it("Handles 2:30", () => {
+    const timeInWords = convertTimeToWords("2:30");
+    expect(timeInWords).toBe("half past two");
+  });
+
+  it("Handles 2:45", () => {
+    const timeInWords = convertTimeToWords("2:45");
+    expect(timeInWords).toBe("quarter to three");
+  });
+
+  it("Handles times between 2:00 - 2:15", () => {
+    const timeInWords = convertTimeToWords("2:03");
+    expect(timeInWords).toBe("three past two");
+  });
+
+  it("Handles times between 2:15 - 2:30", () => {
+    const timeInWords = convertTimeToWords("2:17");
+    expect(timeInWords).toBe("seventeen past two");
+  });
+
+  it("Handles times between 2:30 - 2:45", () => {
+    const timeInWords = convertTimeToWords("2:40");
+    expect(timeInWords).toBe("twenty to three");
+  });
+
+  it("Handles times between 2:45 - 3:00", () => {
+    const timeInWords = convertTimeToWords("2:55");
+    expect(timeInWords).toBe("five to three");
   });
 });
